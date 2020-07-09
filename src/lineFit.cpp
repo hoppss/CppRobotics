fc50e9d0d3958bd4e061205bccf386dcd0508806
hopps.li <hopps.li@cloudminds.com>
2020-07-09 13:49:46 +0800

    linefit

--- src/lineFit.cpp
+++ src/lineFit.cpp
@@ -66,5 +66,31 @@ int main(int argc, char** argv){
 
     //imshow("src", A);
     waitKey(0);
+
+    cout << "fitLine function " << endl;
+
+    cv::Vec4f line_para;
+    cv::fitLine(points, line_para, cv::DIST_L2, 0, 1e-2, 1e-2);
+
+    std::cout << "line_para = " << line_para << std::endl;
+
+    //获取点斜式的点和斜率
+    cv::Point point0;
+    point0.x = line_para[2];
+    point0.y = line_para[3];
+
+    double k = line_para[1] / line_para[0];
+
+    //计算直线的端点(y = k(x - x0) + y0)
+    cv::Point point1, point2;
+    point1.x = 0;
+    point1.y = k * (0 - point0.x) + point0.y;
+    point2.x = 400;
+    point2.y = k * (400 - point0.x) + point0.y;
+
+    cv::line(src, point1, point2, cv::Scalar(0, 255, 0), 2, 8, 0);
+    waitKey(0);
+
+
     return 0;
 }
