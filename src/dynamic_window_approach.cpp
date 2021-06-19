f8cf577560298579932fc18ba8590eab0a18c357
李茂 <limao1@xiaomi.com>
2021-06-19 14:46:25 +0800

    dwa update

--- src/dynamic_window_approach.cpp
+++ src/dynamic_window_approach.cpp
@@ -1,46 +1,54 @@
 /*************************************************************************
-	> File Name: main.cpp
-	> Author: TAI Lei
-	> Mail: ltai@ust.hk
-	> Created Time: Thu Mar  7 19:39:14 2019
+        > File Name: main.cpp
+        > Author: TAI Lei
+        > Mail: ltai@ust.hk
+        > Created Time: Thu Mar  7 19:39:14 2019
  ************************************************************************/
 
-#include<iostream>
-#include<vector>
-#include<array>
-#include<cmath>
-#include<opencv2/opencv.hpp>
-#include<opencv2/core/core.hpp>
-#include<opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <vector>
+#include <array>
+#include <cmath>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #define PI 3.141592653
 
-using Traj = std::vector<std::array<float, 5>>;
-using Obstacle = std::vector<std::array<float, 2>>;
-using State = std::array<float, 5>;
-using Window = std::array<float, 4>;
-using Point = std::array<float, 2>;
-using Control = std::array<float, 2>;
+using State = std::array<float, 5>;  // x，y,theta, vx, vw; 五状态向量
+using Traj = std::vector<State>;     // trajectory or path
 
-class Config{
-public:
+using Point = std::array<float, 2>;
+
+using Obstacle = std::vector<Point>;  // environment map abstract
+
+using Window = std::array<float, 4>;   // two-dims control-state window
+using Control = std::array<float, 2>;  // vx, vw
+
+// config, or its kinematic model
+class Config {
+ public:
   float max_speed = 1.0;
   float min_speed = -0.5;
   float max_yawrate = 40.0 * PI / 180.0;
+
   float max_accel = 0.2;
   float robot_radius = 1.0;
   float max_dyawrate = 40.0 * PI / 180.0;
 
-  float v_reso = 0.01;
-  float yawrate_reso = 0.1 * PI / 180.0;
+  float v_reso = 0.01;                    // window-x [ v_min: v_reso:  v_max]
+  float yawrate_reso = 0.1 * PI / 180.0;  // window-w [w_min: w_reso: w_max ]
 
-  float dt = 0.1;
-  float predict_time = 3.0;
-  float to_goal_cost_gain = 1.0;
+  float dt = 0.1;  // sim_time granularity
+
+  float predict_time = 3.0;  // forward simulation and get traj to be evaluated
+
+  float to_goal_cost_gain = 1.0;  // weighted factor for evalution funciton
   float speed_cost_gain = 1.0;
 };
 
-State motion(State x, Control u, float dt){
+// forward simulation
+State motion(State x, Control u, float dt) {
   x[2] += u[1] * dt;
   x[0] += u[0] * std::cos(x[2]) * dt;
   x[1] += u[0] * std::sin(x[2]) * dt;
@@ -49,23 +57,20 @@ State motion(State x, Control u, float dt){
   return x;
 };
 
-Window calc_dynamic_window(State x, Config config){
-
-  return {{
-    std::max((x[3] - config.max_accel * config.dt), config.min_speed),
-    std::min((x[3] + config.max_accel * config.dt), config.max_speed),
-    std::max((x[4] - config.max_dyawrate * config.dt), -config.max_yawrate),
-    std::min((x[4] + config.max_dyawrate * config.dt), config.max_yawrate)
-  }};
+// windows for control state
+Window calc_dynamic_window(State x, Config config) {
+  return {
+      {std::max((x[3] - config.max_accel * config.dt), config.min_speed),
+       std::min((x[3] + config.max_accel * config.dt), config.max_speed),
+       std::max((x[4] - config.max_dyawrate * config.dt), -config.max_yawrate),
+       std::min((x[4] + config.max_dyawrate * config.dt), config.max_yawrate)}};
 };
 
-
-Traj calc_trajectory(State x, float v, float y, Config config){
-
+Traj calc_trajectory(State x, float v, float y, Config config) {
   Traj traj;
   traj.push_back(x);
   float time = 0.0;
-  while (time <= config.predict_time){
+  while (time <= config.predict_time) {
     x = motion(x, std::array<float, 2>{{v, y}}, config.dt);
     traj.push_back(x);
     time += config.dt;
@@ -73,26 +78,26 @@ Traj calc_trajectory(State x, float v, float y, Config config){
   return traj;
 };
 
-
-float calc_obstacle_cost(Traj traj, Obstacle ob, Config config){
+// 距离障碍物距离， 碰撞检测用, cost 越小越好
+float calc_obstacle_cost(Traj traj, Obstacle ob, Config config) {
   // calc obstacle cost inf: collistion, 0:free
   int skip_n = 2;
   float minr = std::numeric_limits<float>::max();
 
-  for (unsigned int ii=0; ii<traj.size(); ii+=skip_n){
-    for (unsigned int i=0; i< ob.size(); i++){
+  for (unsigned int ii = 0; ii < traj.size(); ii += skip_n) {
+    for (unsigned int i = 0; i < ob.size(); i++) {
       float ox = ob[i][0];
       float oy = ob[i][1];
       float dx = traj[ii][0] - ox;
       float dy = traj[ii][1] - oy;
 
-      float r = std::sqrt(dx*dx + dy*dy);
-      if (r <= config.robot_radius){
-          return std::numeric_limits<float>::max();
+      float r = std::sqrt(dx * dx + dy * dy);
+      if (r <= config.robot_radius) {
+        return std::numeric_limits<float>::max();
       }
 
-      if (minr >= r){
-          minr = r;
+      if (minr >= r) {
+        minr = r;
       }
     }
   }
@@ -100,10 +105,11 @@ float calc_obstacle_cost(Traj traj, Obstacle ob, Config config){
   return 1.0 / minr;
 };
 
-float calc_to_goal_cost(Traj traj, Point goal, Config config){
-
-  float goal_magnitude = std::sqrt(goal[0]*goal[0] + goal[1]*goal[1]);
-  float traj_magnitude = std::sqrt(std::pow(traj.back()[0], 2) + std::pow(traj.back()[1], 2));
+// traj 前向模拟最后一点和目标点， 两个向量之间夹角， 投影cos
+float calc_to_goal_cost(Traj traj, Point goal, Config config) {
+  float goal_magnitude = std::sqrt(goal[0] * goal[0] + goal[1] * goal[1]);
+  float traj_magnitude =
+      std::sqrt(std::pow(traj.back()[0], 2) + std::pow(traj.back()[1], 2));
   float dot_product = (goal[0] * traj.back()[0]) + (goal[1] * traj.back()[1]);
   float error = dot_product / (goal_magnitude * traj_magnitude);
   float error_angle = std::acos(error);
@@ -112,127 +118,149 @@ float calc_to_goal_cost(Traj traj, Point goal, Config config){
   return cost;
 };
 
-Traj calc_final_input(
-  State x, Control& u,
-  Window dw, Config config, Point goal,
-  std::vector<std::array<float, 2>>ob){
+// algrithm core: DWA
+// 1. 根据当前速度u，和其窗口，对控制状态进行采样并生成轨迹
+// 2. 对轨迹进行评价，选出最好的轨迹
+// 3. 返回最好的轨迹，返回最好轨迹的控制
+Traj calc_final_input(State x, Control& u, Window dw, Config config, Point goal,
+                      std::vector<std::array<float, 2>> ob) {
+  float min_cost = 10000.0;
+  Control min_u = u;
+  min_u[0] = 0.0;
+  Traj best_traj;
 
-    float min_cost = 10000.0;
-    Control min_u = u;
-    min_u[0] = 0.0;
-    Traj best_traj;
+  // evalucate all trajectory with sampled input in dynamic window
+  for (float v = dw[0]; v <= dw[1]; v += config.v_reso) {
+    for (float y = dw[2]; y <= dw[3]; y += config.yawrate_reso) {
+      Traj traj = calc_trajectory(x, v, y, config);
 
-    // evalucate all trajectory with sampled input in dynamic window
-    for (float v=dw[0]; v<=dw[1]; v+=config.v_reso){
-      for (float y=dw[2]; y<=dw[3]; y+=config.yawrate_reso){
+      float to_goal_cost = calc_to_goal_cost(traj, goal, config);
 
-        Traj traj = calc_trajectory(x, v, y, config);
+      // 速度越大， cost 越低， forward prefer ?
+      float speed_cost =
+          config.speed_cost_gain * (config.max_speed - traj.back()[3]);
 
-        float to_goal_cost = calc_to_goal_cost(traj, goal, config);
-        float speed_cost = config.speed_cost_gain * (config.max_speed - traj.back()[3]);
-        float ob_cost = calc_obstacle_cost(traj, ob, config);
-        float final_cost = to_goal_cost + speed_cost + ob_cost;
+      float ob_cost = calc_obstacle_cost(traj, ob, config);
 
-        if (min_cost >= final_cost){
-          min_cost = final_cost;
-          min_u = Control{{v, y}};
-          best_traj = traj;
-        }
+      float final_cost = to_goal_cost + speed_cost + ob_cost;
+
+      if (min_cost >= final_cost) {
+        min_cost = final_cost;
+        min_u = Control{{v, y}};
+        best_traj = traj;
       }
     }
-    u = min_u;
-    return best_traj;
+  }
+  u = min_u;
+  return best_traj;
 };
 
+// 最外层算法入口， 返回轨迹为了可视化把
+Traj dwa_control(State x, Control& u, Config config, Point goal, Obstacle ob) {
+  // 1. 基于当前u，生成速度窗口
+  // 2. 基于当前速度u，当前位置x，当前窗口，进行dwa速度分辨率采样-> 生成轨迹 ->
+  // \ 评价轨迹 -> 选出最好轨迹及其控制u -> control
+  Window dw = calc_dynamic_window(x, config);
+  Traj traj = calc_final_input(x, u, dw, config, goal, ob);
 
-Traj dwa_control(State x, Control & u, Config config,
-  Point goal, Obstacle ob){
-    // # Dynamic Window control
-    Window dw = calc_dynamic_window(x, config);
-    Traj traj = calc_final_input(x, u, dw, config, goal, ob);
+  return traj;
+};
 
-    return u, traj;
-  }
-
-cv::Point2i cv_offset(
-    float x, float y, int image_width=2000, int image_height=2000){
+// 可视化，放大，要不然看不清
+cv::Point2i cv_offset(float x, float y, int image_width = 1000,
+                      int image_height = 1000) {
   cv::Point2i output;
-  output.x = int(x * 100) + image_width/2;
-  output.y = image_height - int(y * 100) - image_height/3;
+  output.x = int(x * 50);
+  output.y = int(y * 50);
   return output;
 };
 
+int main() {
+  // 起点终点， x始终表示当前位置点
+  State x({{0.0, 0.0, PI / 8.0, 0.0, 0.0}});
+  Point goal({{10.0, 10.0}});
 
-int main(){
-  State x({{0.0, 0.0, PI/8.0, 0.0, 0.0}});
-  Point goal({{10.0,10.0}});
-  Obstacle ob({
-    {{-1, -1}},
-    {{0, 2}},
-    {{4.0, 2.0}},
-    {{5.0, 4.0}},
-    {{5.0, 5.0}},
-    {{5.0, 6.0}},
-    {{5.0, 9.0}},
-    {{8.0, 9.0}},
-    {{7.0, 9.0}},
-    {{12.0, 12.0}}
-  });
+  // 点障碍物
+  Obstacle ob({{{0, 2}},
+               {{4.0, 2.0}},
+               {{5.0, 4.0}},
+               {{5.0, 5.0}},
+               {{5.0, 6.0}},
+               {{5.0, 9.0}},
+               {{8.0, 9.0}},
+               {{7.0, 9.0}},
+               {{12.0, 12.0}}});
 
   Control u({{0.0, 0.0}});
   Config config;
-  Traj traj;
-  traj.push_back(x);
+  Traj gloal_traj;
+  gloal_traj.push_back(x);
 
   bool terminal = false;
 
-  cv::namedWindow("dwa", cv::WINDOW_NORMAL);
+  cv::namedWindow("dwa", cv::WINDOW_AUTOSIZE);
+
   int count = 0;
 
-  for(int i=0; i<1000 && !terminal; i++){
+  for (int i = 0; i < 1000 && !terminal; i++) {
     Traj ltraj = dwa_control(x, u, config, goal, ob);
     x = motion(x, u, config.dt);
-    traj.push_back(x);
+    gloal_traj.push_back(x);
 
-
-    // visualization
-    cv::Mat bg(3500,3500, CV_8UC3, cv::Scalar(255,255,255));
-    cv::circle(bg, cv_offset(goal[0], goal[1], bg.cols, bg.rows),
-               30, cv::Scalar(255,0,0), 5);
-    for(unsigned int j=0; j<ob.size(); j++){
-      cv::circle(bg, cv_offset(ob[j][0], ob[j][1], bg.cols, bg.rows),
-                 20, cv::Scalar(0,0,0), -1);
+    // visualization - backgroud 初始化为白色背景
+    cv::Mat bg(1000, 1000, CV_8UC3, cv::Scalar(255, 255, 255));
+    // visualization - 蓝色圆圈 为目标点
+    cv::circle(bg, cv_offset(goal[0], goal[1], bg.cols, bg.rows), 30,
+               cv::Scalar(255, 0, 0), 5);
+    // visulizaiton - 障碍物画圆， -1 实心填充
+    for (unsigned int j = 0; j < ob.size(); j++) {
+      cv::circle(bg, cv_offset(ob[j][0], ob[j][1], bg.cols, bg.rows), 20,
+                 cv::Scalar(0, 0, 0), -1);
     }
-    for(unsigned int j=0; j<ltraj.size(); j++){
-      cv::circle(bg, cv_offset(ltraj[j][0], ltraj[j][1], bg.cols, bg.rows),
-                 7, cv::Scalar(0,255,0), -1);
+    // visualzation - 当前最优轨迹可视化，绿色
+    for (unsigned int j = 0; j < ltraj.size(); j++) {
+      cv::circle(bg, cv_offset(ltraj[j][0], ltraj[j][1], bg.cols, bg.rows), 7,
+                 cv::Scalar(0, 255, 0), -1);
     }
-    cv::circle(bg, cv_offset(x[0], x[1], bg.cols, bg.rows),
-               30, cv::Scalar(0,0,255), 5);
+    // visualization - 圈出当前位置点，红色
+    cv::circle(bg, cv_offset(x[0], x[1], bg.cols, bg.rows), 30,
+               cv::Scalar(0, 0, 255), 5);
 
+    // visualization - 直线箭头， 柄： 当前位置x， 箭头：当前速度值*三角映射到xY
+    cv::arrowedLine(bg, cv_offset(x[0], x[1], bg.cols, bg.rows),
+                    cv_offset(x[0] + std::cos(x[2]), x[1] + std::sin(x[2]),
+                              bg.cols, bg.rows),
+                    cv::Scalar(0, 0, 0), 7);
+    // log, output current speed
+    std::cout << "vx: " << x[3] << ", v2: " << x[4] << std::endl;
 
-    cv::arrowedLine(
-      bg,
-      cv_offset(x[0], x[1], bg.cols, bg.rows),
-      cv_offset(x[0] + std::cos(x[2]), x[1] + std::sin(x[2]), bg.cols, bg.rows),
-      cv::Scalar(255,0,255),
-      7);
-
-    if (std::sqrt(std::pow((x[0] - goal[0]), 2) + std::pow((x[1] - goal[1]), 2)) <= config.robot_radius){
+    // isGoalReached judge
+    if (std::sqrt(std::pow((x[0] - goal[0]), 2) +
+                  std::pow((x[1] - goal[1]), 2)) <= config.robot_radius) {
       terminal = true;
-      for(unsigned int j=0; j<traj.size(); j++){
-        cv::circle(bg, cv_offset(traj[j][0], traj[j][1], bg.cols, bg.rows),
-                    7, cv::Scalar(0,0,255), -1);
+      for (unsigned int j = 0; j < gloal_traj.size(); j++) {
+        cv::circle(
+            bg, cv_offset(gloal_traj[j][0], gloal_traj[j][1], bg.cols, bg.rows),
+            7, cv::Scalar(0, 0, 255), -1);
       }
     }
 
-
     cv::imshow("dwa", bg);
-    cv::waitKey(5);
+
+    if (terminal)
+      cv::waitKey(3000);
+    else
+      cv::waitKey(30);
 
     // std::string int_count = std::to_string(count);
-    // cv::imwrite("./pngs/"+std::string(5-int_count.length(), '0').append(int_count)+".png", bg);
+    // cv::imwrite("./pngs/"+std::string(5-int_count.length(),
+    // '0').append(int_count)+".png", bg);
 
     count++;
   }
 }
+
+/**
+ * 1. vx 增长到最大后，一直保持在max 附近
+ * 2. vw 是有效的
+ * */
